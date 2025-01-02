4ae2795f10a029935b2d0e674baf6f647e8feed0
Lucio Zhao <luc.dev1008@gmail.com>
2025-01-01 19:03:03 -0500

    Refactors project
    - refactors naming convention
    - refactors folder structure

--- src/main/java/com/lucio/leetcode/easy/Problem1_TwoSum.java
+++ src/main/java/com/lucio/leetcode/easy/Problem1_TwoSum.java
@@ -1,21 +1,19 @@
-package com.lucio.leetcode.a_ts;
+package com.lucio.leetcode.easy;
 
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Problem 1: https://leetcode.com/problems/two-sum
+ * Problem 1: <a href="https://leetcode.com/problems/two-sum">LeetCode</a>
  * <p>
  * Given an array of integers numbers and an integer target, return indices of the two numbers such that they add up to
  * target.
- * </p>
  * <p>
  * You may assume that each input would have exactly one solution, and you may not use the same element twice.
  * You can return the answer in any order.
- * </p>
  */
-public class TwoSum {
+public class Problem1_TwoSum {
     public static void main(String[] args) {
         int[] numbers = {1, 4, 6, 3, 7, 19};
 
